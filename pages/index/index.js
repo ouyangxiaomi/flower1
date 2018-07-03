263666e714c8533fef212b47d7d51366a5f65747
ouyang <1464837044@qq.com>
2018-07-03 15:48:36 +0800

    麦上

---
+++ pages/index/index.js
@@ -0,0 +1,12 @@
+Page({
+  data: {
+    imgUrls: [
+      '../../images/img-1.jpg',
+      '../../images/img-2.jpg',
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000
+  }
+})
